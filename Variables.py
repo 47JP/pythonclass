483f56d9ba74580e95280d4dc57e3e31e298f630
47JP <jayantvjp47@gmail.com>
2025-01-21 17:58:04 -0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,53 @@
+#multiplaction1 in python
+
+value1 = int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+
+multiply = value1 * value2
+
+print("The answer to your query is: {}" .format(multiply))
+
+#division1 in python
+
+value1 = int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+
+divide = value1 / value2
+
+print("The answer to your query is: {}" .format(divide))
+
+#division2 in python
+
+value1 = int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+
+divide2 = value1 // value2
+
+print("The answer to your query is: {}" .format(divide2))
+
+#modulo in python
+
+value1 = int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+
+modulo = value1 % value2
+
+print("The answer to your query is: {}" .format(modulo))
+
+#addition in python
+
+value1 = int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+
+add = value1 + value2
+
+print("The answer to your query is: {}" .format(add))
+
+#substraction in python
+
+value1 = int(input("Please enter a number\n"))
+value2 = int(input("Please enter another number\n"))
+
+substract = value1 - value2
+
+print("The answer to your query is: {}" .format(substract))
